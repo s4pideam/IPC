c566206586b05466193dc10876db744f138ef717
Marlin Zapp <s4mazapp@uni-trier.de>
2024-04-10 09:16:49 +0200

    implement named pipes.

--- src/main/java/org/betriebssysteme/Interfaces/IIPCServer.java
+++ src/main/java/org/betriebssysteme/Interfaces/IIPCServer.java
@@ -1,9 +1,18 @@
 package org.betriebssysteme.Interfaces;
 
+import java.io.DataOutputStream;
 import java.util.Map;
+
+import org.betriebssysteme.Enum.EPackage;
 
 public interface IIPCServer {
     void start();
+
     void stop();
+
+    void send(DataOutputStream dataOutputStream, EPackage epackage, String message);
+
     void init(Map<String, Object> configMap);
+
+    void updateWordCount(String word, int count);
 }
--- src/main/java/org/betriebssysteme/Utils/Utils.java
+++ src/main/java/org/betriebssysteme/Utils/Utils.java
@@ -1,5 +1,8 @@
 package org.betriebssysteme.Utils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 
 public class Utils {
@@ -15,11 +18,35 @@ public class Utils {
         String[] words = message.toLowerCase().split("\\W+");
 
         for (String word : words) {
-            if (!word.matches("[a-z]+"))
+            if (!word.matches("[a-z]+")) {
                 continue;
+            }
             String key = String.valueOf(word.charAt(0));
             wordCount.computeIfAbsent(key, k -> new HashMap<>());
             wordCount.get(key).merge(word, 1, Integer::sum);
+        }
+    }
+
+    public static void createNamedPipe(Path path) {
+        try {
+            if (!Files.exists(path)) {
+                // Create a named pipe using the mkfifo command
+                ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c", "mkfifo " + path);
+                Process process = processBuilder.start();
+
+                // Wait for the process to complete
+                int exitCode = process.waitFor();
+
+                if (exitCode == 0) {
+                    System.out.println("Named pipe created successfully");
+                } else {
+                    System.out.println("Error creating named pipe");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
